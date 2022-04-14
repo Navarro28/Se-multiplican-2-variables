fbc60f6585a90d0a0c97bfe05bf12bf1f2b66f22
jose.raul.navarro.avila27@uabc.edu.mx <jose.raul.navarro.avila27@uabc.edu.mx>
2022-04-13 21:28:57 -0700

    Agregar archivos de proyecto.

---
+++ Operadores aritmeticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 10;
+            int numero2 = 5;
+
+            Console.WriteLine(numero1 * numero2);
+            Console.ReadKey();
+        }
+    }
+}
